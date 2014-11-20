aba9422414942f6a094c288325739918434560b0
watauchi <watauchi@APPLE20130730-no-MacBook-Pro.local>
2014-11-20 18:50:45 +0900

    ユーザーモデル作成 セキュアパスまで

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,87 +2,58 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-<<<<<<< HEAD
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
+  end
 
-  describe "Home page" do
-=======
+
   subject { page }
->>>>>>> add-layouts
+
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
   describe "Home page" do
     before { visit root_path }
 
-<<<<<<< HEAD
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
-    end
-=======
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
->>>>>>> add-layouts
   end
 
   describe "Help page" do
     before { visit help_path }
 
-<<<<<<< HEAD
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
-    end
-=======
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
->>>>>>> add-layouts
   end
 
   describe "About page" do
     before { visit about_path }
 
-<<<<<<< HEAD
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About Us")
-    end
-  end
-
-  describe "Contact page" do
-
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} | Contact")
-    end
-  end
-end
-
-=======
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
+    let(:heading)    { 'About' }
+    let(:page_title) { 'About Us' }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    let(:heading)    { 'Contact' }
+    let(:page_title) { 'Contact' }
   end
 end
->>>>>>> add-layouts
